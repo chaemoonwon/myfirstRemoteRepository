2ff001dec6e7ec5016a56fa855d3d3af77ed435b
cmoonth47 <cmoonth47@gmail.com>
2023-09-06 17:16:54 +0900

    Arraylist 테스트 _ version 1_ 9_6_에 만듬

---
+++ Test1.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.ArrayList;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		
+		
+		ArrayList<Integer> arrayList=new ArrayList<Integer>();
+		
+		int tot = 0;
+		
+		arrayList.add(30);
+		
+		arrayList.add(40);
+		
+		
+		
+		for(int value:arrayList) {
+			tot += value;
+		}
+		
+		System.out.println(tot);
+	}
+
+}
